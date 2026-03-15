f91af38a3d41d8c6ee7e8c6d7047caef0c584553
k0michi <k0michi@koyomi.co>
2026-03-15 17:01:32 +0900

    feat: implement TypeAnnotationNodeHelper

---
+++ src/main/java/com/koyomiji/asmpatch/TypeAnnotationNodeHelper.java
@@ -0,0 +1,23 @@
+package com.koyomiji.asmpatch;
+
+import org.objectweb.asm.tree.TypeAnnotationNode;
+
+import java.util.Objects;
+
+public class TypeAnnotationNodeHelper {
+  public static boolean equals(TypeAnnotationNode a, TypeAnnotationNode b) {
+    if (!AnnotationNodeHelper.equals(a, b)) {
+      return false;
+    }
+
+    if (!Objects.equals(a.typeRef, b.typeRef)) {
+      return false;
+    }
+
+    if (!TypePathHelper.equals(a.typePath, b.typePath)) {
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ src/main/java/com/koyomiji/asmpatch/TypePathHelper.java
@@ -0,0 +1,25 @@
+package com.koyomiji.asmpatch;
+
+import org.objectweb.asm.TypePath;
+
+public class TypePathHelper {
+  public static boolean equals(TypePath a, TypePath b) {
+    if (a.getLength() != b.getLength()) {
+      return false;
+    }
+
+    for (int i = 0; i < a.getLength(); i++) {
+      if (a.getStep(i) != b.getStep(i)) {
+        return false;
+      }
+
+      if (a.getStep(i) == TypePath.TYPE_ARGUMENT) {
+        if (a.getStepArgument(i) != b.getStepArgument(i)) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
