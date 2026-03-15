9952392c05255c63e3c0643602d4be6efa0918b6
k0michi <k0michi@koyomi.co>
2026-03-15 17:09:39 +0900

    fix: add ListHelper.equals

--- src/main/java/com/koyomiji/asmpatch/ListHelper.java
+++ src/main/java/com/koyomiji/asmpatch/ListHelper.java
@@ -6,4 +6,26 @@ public class ListHelper {
   public static <T> List<T> orEmpty(List<T> list) {
     return list != null ? list : List.of();
   }
+
+  public static <T> boolean equals(List<T> a, List<T> b, IEquator<T> equator) {
+    if (a == b) {
+      return true;
+    }
+
+    if (a == null || b == null) {
+      return false;
+    }
+
+    if (a.size() != b.size()) {
+      return false;
+    }
+
+    for (int i = 0; i < a.size(); i++) {
+      if (!equator.equals(a.get(i), b.get(i))) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
