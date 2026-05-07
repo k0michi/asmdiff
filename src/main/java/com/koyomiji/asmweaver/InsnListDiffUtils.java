9a081a7ef66c24005d55c08de6b31734b965c47d
k0michi <k0michi@koyomi.co>
2026-05-07 11:22:23 +0900

    feat: add FuzzyDistanceHeuristic

--- src/main/java/com/koyomiji/asmweaver/InsnListDiffUtils.java
+++ src/main/java/com/koyomiji/asmweaver/InsnListDiffUtils.java
@@ -1,5 +1,6 @@
 package com.koyomiji.asmweaver;
 
+import com.koyomiji.asmweaver.util.BiHashMap;
 import com.koyomiji.asmweaver.util.tuple.Pair;
 import com.koyomiji.asmweaver.util.PeekableIterator;
 import org.objectweb.asm.tree.*;
@@ -465,5 +466,43 @@ public class InsnListDiffUtils {
     result.addAll(mergeInsertionSlot(ins1, ins2));
 
     return new InsnListDiff(result);
+  }
+
+  private static abstract class Heuristic {
+    public abstract int calculate(int indexA, int indexB, BiHashMap<AbstractInsnNode, AbstractInsnNode> labels);
+  }
+
+  private static class FuzzyDistanceHeuristic extends Heuristic {
+    private int[][] table;
+
+    public FuzzyDistanceHeuristic(List<AbstractInsnNode> insnsA, List<AbstractInsnNode> insnsB) {
+      this.table = new int[insnsA.size() + 1][insnsB.size() + 1];
+
+      for (int j = 0; j <= insnsB.size(); j++) {
+        table[insnsA.size()][j] = insnsB.size() - j;
+      }
+
+      for (int i = 0; i <= insnsA.size(); i++) {
+        table[i][insnsB.size()] = insnsA.size() - i;
+      }
+
+      for (int i = insnsA.size() - 1; i >= 0; i--) {
+        for (int j = insnsB.size() - 1; j >= 0; j--) {
+          AbstractInsnNode a = insnsA.get(i);
+          AbstractInsnNode b = insnsB.get(j);
+
+          table[i][j] = 1 + Math.min(table[i + 1][j], table[i][j + 1]);
+
+          if (compareInsnsIgnoreLabels(a, b)) {
+            table[i][j] = Math.min(table[i][j], table[i + 1][j + 1]);
+          }
+        }
+      }
+    }
+
+    @Override
+    public int calculate(int indexA, int indexB, BiHashMap<AbstractInsnNode, AbstractInsnNode> labels) {
+      return table[indexA][indexB];
+    }
   }
 }
