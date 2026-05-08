11618d07dbf4c0055c81e71307be0d304031c071
k0michi <k0michi@koyomi.co>
2026-05-08 16:14:15 +0900

    feat: BiFunctionHelper

---
+++ src/main/java/com/koyomiji/asmweaver/util/BiFunctionHelper.java
@@ -0,0 +1,20 @@
+package com.koyomiji.asmweaver.util;
+
+import com.koyomiji.asmweaver.util.tuple.Pair;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+
+public class BiFunctionHelper {
+  public static <K1, K2, V> BiFunction<K1, K2, V> fromMap(Map<Pair<K1, K2>, V> map) {
+    return (k1, k2) -> map.get(new Pair<>(k1, k2));
+  }
+
+  public static <K2, V> BiFunction<V, K2, V> first() {
+    return (k1, k2) -> (V) k1;
+  }
+
+  public static <K1, V> BiFunction<K1, V, V> second() {
+    return (k1, k2) -> (V) k2;
+  }
+}
