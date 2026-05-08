0453bdf4765b2871eaf8afb0b78bc382035ed3e6
k0michi <k0michi@koyomi.co>
2026-05-08 10:37:15 +0900

    feat: ListDiffUtils

---
+++ src/main/java/com/koyomiji/asmweaver/ListDiffUtils.java
@@ -0,0 +1,163 @@
+package com.koyomiji.asmweaver;
+
+import com.koyomiji.asmweaver.util.PeekableIterator;
+import com.koyomiji.asmweaver.util.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+public class ListDiffUtils {
+  public static <T> ListDiff<T> invert(ListDiff<T> diff) {
+    List<ListDiff.Operation<T>> invertedOperations = new ArrayList<>();
+
+    for (ListDiff.Operation<T> op : diff.operations) {
+      ListDiff.Operation<T> invertedOp;
+
+      switch (op.type) {
+        case MATCH:
+          invertedOp = new ListDiff.Operation<>(ListDiff.Operation.Type.MATCH, null, op.operand);
+          break;
+        case INSERT:
+          invertedOp = new ListDiff.Operation<>(ListDiff.Operation.Type.DELETE, op.mode, op.operand);
+          break;
+        case DELETE:
+          invertedOp = new ListDiff.Operation<>(ListDiff.Operation.Type.INSERT, op.mode, op.operand);
+          break;
+        default:
+          throw new IllegalStateException("Unexpected operation type: " + op.type);
+      }
+
+      invertedOperations.add(invertedOp);
+    }
+
+    return new ListDiff<>(invertedOperations);
+  }
+
+  public static <T> Pair<ListDiff<T>, ListDiff<T>> commute(ListDiff<T> p, ListDiff<T> q, BiPredicate<T, T> compare) throws ConflictException {
+    List<ListDiff.Operation<T>> qPrimeOps = new ArrayList<>();
+    List<ListDiff.Operation<T>> pPrimeOps = new ArrayList<>();
+
+    Iterator<ListDiff.Operation<T>> itP = p.operations.iterator();
+    PeekableIterator<ListDiff.Operation<T>> itQ = new PeekableIterator<>(q.operations.iterator());
+
+    while (itP.hasNext()) {
+      ListDiff.Operation<T> opP = itP.next();
+
+      if (opP.type == ListDiff.Operation.Type.DELETE) {
+        qPrimeOps.add(new ListDiff.Operation<>(ListDiff.Operation.Type.MATCH, opP.mode, opP.operand));
+        pPrimeOps.add(new ListDiff.Operation<>(ListDiff.Operation.Type.DELETE, opP.mode, opP.operand));
+      } else if (opP.type == ListDiff.Operation.Type.MATCH || opP.type == ListDiff.Operation.Type.INSERT) {
+
+        while (itQ.hasNext() && itQ.peek().type == ListDiff.Operation.Type.INSERT) {
+          ListDiff.Operation<T> opQIns = itQ.next();
+
+          qPrimeOps.add(opQIns);
+          pPrimeOps.add(new ListDiff.Operation<>(ListDiff.Operation.Type.MATCH, opQIns.mode, opQIns.operand));
+        }
+
+        if (!itQ.hasNext()) {
+          throw new IllegalDiffException("p has remaining operations after q is exhausted");
+        }
+        ListDiff.Operation<T> opQBase = itQ.next();
+
+        if (!compare.test(opP.operand, opQBase.operand)) {
+          throw new IllegalDiffException("p and q disagree on node identity");
+        }
+
+        if (opP.type == ListDiff.Operation.Type.MATCH) {
+          qPrimeOps.add(opQBase);
+          if (opQBase.type == ListDiff.Operation.Type.MATCH) {
+            pPrimeOps.add(new ListDiff.Operation<>(ListDiff.Operation.Type.MATCH, opP.mode, opP.operand));
+          }
+        } else {
+          if (opQBase.type == ListDiff.Operation.Type.DELETE) {
+            throw new ConflictException("p inserts a node that q deletes");
+          }
+
+          pPrimeOps.add(new ListDiff.Operation<>(opP.type, opP.mode, opP.operand));
+        }
+      }
+    }
+
+    while (itQ.hasNext()) {
+      ListDiff.Operation<T> opQ = itQ.next();
+
+      if (opQ.type == ListDiff.Operation.Type.INSERT) {
+        qPrimeOps.add(opQ);
+        pPrimeOps.add(new ListDiff.Operation<>(ListDiff.Operation.Type.MATCH, opQ.mode, opQ.operand));
+      } else {
+        throw new IllegalDiffException("q has remaining operations after p is exhausted");
+      }
+    }
+
+    return new Pair<>(new ListDiff<>(qPrimeOps), new ListDiff<>(pPrimeOps));
+  }
+
+  public static <T> ListDiff<T> diff(List<T> list1, List<T> list2, BiPredicate<T, T> compare) {
+    int[][] dp = new int[list1.size() + 1][list2.size() + 1];
+
+    for (int i = 0; i <= list1.size(); i++) {
+      dp[i][0] = i;
+    }
+
+    for (int j = 0; j <= list2.size(); j++) {
+      dp[0][j] = j;
+    }
+
+    for (int i = 1; i <= list1.size(); i++) {
+      for (int j = 1; j <= list2.size(); j++) {
+        if (compare.test(list1.get(i - 1), list2.get(j - 1))) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+        }
+      }
+    }
+
+    List<ListDiff.Operation<T>> operations = new ArrayList<>();
+
+    int i = list1.size();
+    int j = list2.size();
+
+    while (i > 0 && j > 0) {
+      if (compare.test(list1.get(i - 1), list2.get(j - 1))) {
+        operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.MATCH, null, list1.get(i - 1)));
+        i--;
+        j--;
+//      } else if (dp[i][j] == dp[i - 1][j] + 1) {
+//        operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.DELETE, null, list1.get(i - 1)));
+//        i--;
+//      } else {
+//        operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.INSERT, null, list2.get(j - 1)));
+//        j--;
+//      }
+      } else if (dp[i][j] == dp[i][j - 1] + 1) {
+        operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.INSERT, null, list2.get(j - 1)));
+        j--;
+      } else {
+        operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.DELETE, null, list1.get(i - 1)));
+        i--;
+      }
+    }
+
+    while (i > 0) {
+      operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.DELETE, null, list1.get(i - 1)));
+      i--;
+    }
+
+    while (j > 0) {
+      operations.add(new ListDiff.Operation<>(ListDiff.Operation.Type.INSERT, null, list2.get(j - 1)));
+      j--;
+    }
+
+    List<ListDiff.Operation<T>> reversedOperations = new ArrayList<>();
+
+    for (int k = operations.size() - 1; k >= 0; k--) {
+      reversedOperations.add(operations.get(k));
+    }
+
+    return new ListDiff<>(reversedOperations);
+  }
+}
---
+++ src/test/java/com/koyomiji/asmweaver/ListDiffUtilsTest.java
@@ -0,0 +1,81 @@
+package com.koyomiji.asmweaver;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class ListDiffUtilsTest {
+  @Test
+  void test_diff_0() {
+    var oldList = List.of(1, 2, 3);
+    var newList = List.of(1, 2, 3);
+    var diff = ListDiffUtils.diff(oldList, newList, Integer::equals);
+    Assertions.assertEquals(3, diff.operations.size());
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, diff.operations.get(0).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, diff.operations.get(1).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, diff.operations.get(2).type);
+  }
+
+  @Test
+  void test_diff_1() {
+    var oldList = List.of(1, 2, 3);
+    var newList = List.of(1, 4, 3);
+    var diff = ListDiffUtils.diff(oldList, newList, Integer::equals);
+    Assertions.assertEquals(4, diff.operations.size());
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, diff.operations.get(0).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.DELETE, diff.operations.get(1).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.INSERT, diff.operations.get(2).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, diff.operations.get(3).type);
+  }
+
+  @Test
+  void test_diff_2() {
+    List<Integer> oldList = List.of();
+    var newList = List.of(4, 5, 6);
+    var diff = ListDiffUtils.diff(oldList, newList, Integer::equals);
+    Assertions.assertEquals(3, diff.operations.size());
+    Assertions.assertEquals(ListDiff.Operation.Type.INSERT, diff.operations.get(0).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.INSERT, diff.operations.get(1).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.INSERT, diff.operations.get(2).type);
+  }
+
+  @Test
+  void test_diff_3() {
+    var oldList = List.of(1, 2, 3);
+    List<Integer> newList = List.of();
+    var diff = ListDiffUtils.diff(oldList, newList, Integer::equals);
+    Assertions.assertEquals(3, diff.operations.size());
+    Assertions.assertEquals(ListDiff.Operation.Type.DELETE, diff.operations.get(0).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.DELETE, diff.operations.get(1).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.DELETE, diff.operations.get(2).type);
+  }
+
+  @Test
+  void test_commute_0() throws ConflictException {
+    var list1 = List.of(1, 2, 3);
+    var list2 = List.of(2, 2, 3);
+    var list3 = List.of(2, 2, 4);
+
+    var diff12 = ListDiffUtils.diff(list1, list2, Integer::equals);
+    var diff23 = ListDiffUtils.diff(list2, list3, Integer::equals);
+
+    var commuted = ListDiffUtils.commute(diff12, diff23, Integer::equals);
+    var commuted1 = commuted.first;
+    var commuted2 = commuted.second;
+
+    Assertions.assertEquals(4, commuted1.operations.size());
+    // 1 2 3 -> 1 2 4
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, commuted1.operations.get(0).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, commuted1.operations.get(1).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.DELETE, commuted1.operations.get(2).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.INSERT, commuted1.operations.get(3).type);
+
+    Assertions.assertEquals(4, commuted2.operations.size());
+    // 1 2 4 -> 2 2 4
+    Assertions.assertEquals(ListDiff.Operation.Type.DELETE, commuted2.operations.get(0).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.INSERT, commuted2.operations.get(1).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, commuted2.operations.get(2).type);
+    Assertions.assertEquals(ListDiff.Operation.Type.MATCH, commuted2.operations.get(3).type);
+  }
+}
