2fc3df9a727e5fc702b9ed1af35ee4f2ab722482
k0michi <k0michi@koyomi.co>
2026-05-08 15:12:50 +0900

    feat: TryCatchBlockNodeHelper

---
+++ src/main/java/com/koyomiji/asmweaver/TryCatchBlockNodeHelper.java
@@ -0,0 +1,30 @@
+package com.koyomiji.asmweaver;
+
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.TryCatchBlockNode;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+public class TryCatchBlockNodeHelper {
+  public static boolean equals(TryCatchBlockNode node1, TryCatchBlockNode node2, Function<LabelNode, LabelNode> labelMap) {
+    if (node1 == node2) {
+      return true;
+    }
+
+    if (node1 == null || node2 == null) {
+      return false;
+    }
+
+    return labelMap.apply(node1.start) == labelMap.apply(node2.start)
+            && labelMap.apply(node1.end) == labelMap.apply(node2.end)
+            && labelMap.apply(node1.handler) == labelMap.apply(node2.handler)
+            && Objects.equals(node1.type, node2.type)
+            && ListHelper.equals(node1.visibleTypeAnnotations, node2.visibleTypeAnnotations, TypeAnnotationNodeHelper::equals)
+            && ListHelper.equals(node1.invisibleTypeAnnotations, node2.invisibleTypeAnnotations, TypeAnnotationNodeHelper::equals);
+  }
+
+  public static boolean equals(TryCatchBlockNode node1, TryCatchBlockNode node2) {
+    return equals(node1, node2, Function.identity());
+  }
+}
---
+++ src/test/java/com/koyomiji/asmweaver/TryCatchBlockNodeHelperTest.java
@@ -0,0 +1,65 @@
+package com.koyomiji.asmweaver;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.objectweb.asm.tree.LabelNode;
+import org.objectweb.asm.tree.TryCatchBlockNode;
+
+class TryCatchBlockNodeHelperTest {
+  LabelNode l0 = new LabelNode();
+  LabelNode l1 = new LabelNode();
+  LabelNode l2 = new LabelNode();
+  LabelNode l3 = new LabelNode();
+
+  @Test
+  void test_equals_0() {
+    TryCatchBlockNode node1 = new TryCatchBlockNode(l0, l1, l2, "java/lang/Exception");
+    TryCatchBlockNode node2 = new TryCatchBlockNode(l0, l1, l2, "java/lang/Exception");
+    Assertions.assertTrue(TryCatchBlockNodeHelper.equals(node1, node2));
+  }
+
+  @Test
+  void test_equals_1() {
+    TryCatchBlockNode node1 = new TryCatchBlockNode(l0, l1, l2, "java/lang/Exception");
+    TryCatchBlockNode node2 = new TryCatchBlockNode(l0, l1, l2, "java/lang/RuntimeException");
+    Assertions.assertFalse(TryCatchBlockNodeHelper.equals(node1, node2));
+  }
+
+  @Test
+  void test_equals_2() {
+    TryCatchBlockNode node1 = new TryCatchBlockNode(l0, l1, l2, "java/lang/Exception");
+    Assertions.assertFalse(TryCatchBlockNodeHelper.equals(node1, null));
+  }
+
+  @Test
+  void test_equals_3() {
+    TryCatchBlockNode node2 = new TryCatchBlockNode(l0, l1, l2, "java/lang/Exception");
+    Assertions.assertFalse(TryCatchBlockNodeHelper.equals(null, node2));
+  }
+
+  @Test
+  void test_equals_4() {
+    Assertions.assertTrue(TryCatchBlockNodeHelper.equals(null, null));
+  }
+
+  @Test
+  void test_equals_5() {
+    TryCatchBlockNode node1 = new TryCatchBlockNode(l0, l1, l2, null);
+    TryCatchBlockNode node2 = new TryCatchBlockNode(l1, l1, l2, null);
+    Assertions.assertFalse(TryCatchBlockNodeHelper.equals(node1, node2));
+  }
+
+  @Test
+  void test_equals_6() {
+    TryCatchBlockNode node1 = new TryCatchBlockNode(l0, l1, l2, null);
+    TryCatchBlockNode node2 = new TryCatchBlockNode(l0, l2, l2, null);
+    Assertions.assertFalse(TryCatchBlockNodeHelper.equals(node1, node2));
+  }
+
+  @Test
+  void test_equals_7() {
+    TryCatchBlockNode node1 = new TryCatchBlockNode(l0, l1, l2, null);
+    TryCatchBlockNode node2 = new TryCatchBlockNode(l0, l1, l3, null);
+    Assertions.assertFalse(TryCatchBlockNodeHelper.equals(node1, node2));
+  }
+}
