ad588fa5a3556779c075a28e0916a29cbff03e6c
k0michi <k0michi@koyomi.co>
2026-05-08 15:00:32 +0900

    style: format

--- src/main/java/com/koyomiji/asmweaver/InnerClassNodeHelper.java
+++ src/main/java/com/koyomiji/asmweaver/InnerClassNodeHelper.java
@@ -9,12 +9,14 @@ public class InnerClassNodeHelper {
     if (node1 == node2) {
       return true;
     }
+
     if (node1 == null || node2 == null) {
       return false;
     }
+
     return Objects.equals(node1.name, node2.name)
-        && Objects.equals(node1.outerName, node2.outerName)
-        && Objects.equals(node1.innerName, node2.innerName)
-        && node1.access == node2.access;
+            && Objects.equals(node1.outerName, node2.outerName)
+            && Objects.equals(node1.innerName, node2.innerName)
+            && node1.access == node2.access;
   }
 }
