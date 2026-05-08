7ebebda3165706bf6ac777cbec30259c0e35424e
k0michi <k0michi@koyomi.co>
2026-05-08 15:28:26 +0900

    fix: ditch IEquator

--- src/main/java/com/koyomiji/asmweaver/ListHelper.java
+++ src/main/java/com/koyomiji/asmweaver/ListHelper.java
@@ -1,20 +1,19 @@
 package com.koyomiji.asmweaver;
 
-import com.koyomiji.asmpatch.IEquator;
-
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiPredicate;
 
 public class ListHelper {
   public static <T> List<T> orEmpty(List<T> list) {
     return list != null ? list : List.of();
   }
 
-  public static<T> List<T> ofNullable(T element) {
+  public static <T> List<T> ofNullable(T element) {
     return element != null ? List.of(element) : List.of();
   }
 
-  public static <T> boolean equals(List<T> a, List<T> b, IEquator<T> equator) {
+  public static <T> boolean equals(List<T> a, List<T> b, BiPredicate<T, T> compare) {
     if (a == b) {
       return true;
     }
@@ -28,7 +27,7 @@ public class ListHelper {
     }
 
     for (int i = 0; i < a.size(); i++) {
-      if (!equator.equals(a.get(i), b.get(i))) {
+      if (!compare.test(a.get(i), b.get(i))) {
         return false;
       }
     }
