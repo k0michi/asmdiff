d090160aab422d93971a5bc4b09d29a4e96a4520
k0michi <k0michi@koyomi.co>
2026-05-08 16:03:33 +0900

    feat: Pair equals/hashCode

--- src/main/java/com/koyomiji/asmweaver/util/tuple/Pair.java
+++ src/main/java/com/koyomiji/asmweaver/util/tuple/Pair.java
@@ -1,5 +1,7 @@
 package com.koyomiji.asmweaver.util.tuple;
 
+import java.util.Objects;
+
 public class Pair<A, B> {
   public final A first;
   public final B second;
@@ -7,5 +9,18 @@ public class Pair<A, B> {
   public Pair(A first, B second) {
     this.first = first;
     this.second = second;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Pair<?, ?> pair = (Pair<?, ?>) o;
+    return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(first, second);
   }
 }
