0b8cb716c37d873d9b53187366f2b3d0eb64650b
k0michi <k0michi@koyomi.co>
2026-05-11 13:50:29 +0900

    feat: RecordComponentDiff.isEmpty

--- src/main/java/com/koyomiji/asmweaver/RecordComponentDiff.java
+++ src/main/java/com/koyomiji/asmweaver/RecordComponentDiff.java
@@ -3,7 +3,7 @@ package com.koyomiji.asmweaver;
 import org.objectweb.asm.tree.AnnotationNode;
 import org.objectweb.asm.tree.TypeAnnotationNode;
 
-public class RecordComponentDiff {
+public class RecordComponentDiff implements IDiff {
   public ListDiff<String> name;
   public ListDiff<String> descriptor;
   public ListDiff<String> signature;
@@ -12,4 +12,15 @@ public class RecordComponentDiff {
   public ListDiff<TypeAnnotationNode> visibleTypeAnnotations;
   public ListDiff<TypeAnnotationNode> invisibleTypeAnnotations;
   // attrs
+
+  @Override
+  public boolean isEmpty() {
+    return name.isEmpty()
+            && descriptor.isEmpty()
+            && signature.isEmpty()
+            && visibleAnnotations.isEmpty()
+            && invisibleAnnotations.isEmpty()
+            && visibleTypeAnnotations.isEmpty()
+            && invisibleTypeAnnotations.isEmpty();
+  }
 }
