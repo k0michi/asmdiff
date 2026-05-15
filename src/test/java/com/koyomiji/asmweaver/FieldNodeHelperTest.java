7dddf9ddcbebeb49b526abf7360a2b3f99c6c278
k0michi <k0michi@koyomi.co>
2026-05-15 20:17:11 +0900

    test: add

--- src/test/java/com/koyomiji/asmweaver/FieldNodeHelperTest.java
+++ src/test/java/com/koyomiji/asmweaver/FieldNodeHelperTest.java
@@ -15,7 +15,10 @@ public class FieldNodeHelperTest {
             new FieldNode(0, "field2", "I", null, null),
             new FieldNode(0, "field1", "J", null, null),
             new FieldNode(0, "field1", "I", "Ljava/lang/Object;", null),
-            new FieldNode(0, "field1", "I", null, 123)
+            new FieldNode(0, "field1", "I", null, 123),
+            new FieldNode(0, "field1", "I", null, (float) 123),
+            new FieldNode(0, "field1", "I", null, (double) 123),
+            new FieldNode(0, "field1", "I", null, "a")
     );
   }
 
