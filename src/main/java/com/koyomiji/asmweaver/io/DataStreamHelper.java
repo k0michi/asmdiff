4dc81b1d445327c4f781908e2c024c4224160f68
k0michi <k0michi@koyomi.co>
2026-05-15 11:20:01 +0900

    feat: readUTFNullable/writeUTFNullable

---
+++ src/main/java/com/koyomiji/asmweaver/io/DataStreamHelper.java
@@ -0,0 +1,25 @@
+package com.koyomiji.asmweaver.io;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class DataStreamHelper {
+  public static String readUTFNullable(java.io.DataInputStream stream) throws java.io.IOException {
+    boolean isNotNull = stream.readBoolean();
+
+    if (isNotNull) {
+      return stream.readUTF();
+    } else {
+      return null;
+    }
+  }
+
+  public static void writeUTFNullable(DataOutputStream stream, String value) throws IOException {
+    if (value == null) {
+      stream.writeBoolean(false);
+    } else {
+      stream.writeBoolean(true);
+      stream.writeUTF(value);
+    }
+  }
+}
---
+++ src/test/java/com/koyomiji/asmweaver/io/DataStreamHelperTest.java
@@ -0,0 +1,29 @@
+package com.koyomiji.asmweaver.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class DataStreamHelperTest {
+  @Test
+  void test_readUTFNullable() throws java.io.IOException {
+    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    DataOutputStream dataOutputStream = new DataOutputStream(byteArrayOutputStream);
+
+    DataStreamHelper.writeUTFNullable(dataOutputStream, "Hello, World!");
+    DataStreamHelper.writeUTFNullable(dataOutputStream, null);
+
+    ByteArrayInputStream byteArrayInputStream = new java.io.ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+    DataInputStream dataInputStream = new java.io.DataInputStream(byteArrayInputStream);
+
+    String value1 = DataStreamHelper.readUTFNullable(dataInputStream);
+    String value2 = DataStreamHelper.readUTFNullable(dataInputStream);
+
+    Assertions.assertEquals("Hello, World!", value1);
+    Assertions.assertNull(value2);
+  }
+}
