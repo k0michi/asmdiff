f1179475e3e2fc7c9b2621db1d5e02d71000e907
k0michi <k0michi@koyomi.co>
2026-05-16 10:49:24 +0900

    feat: AutoIncrementBiHashMap

---
+++ src/main/java/com/koyomiji/asmweaver/AutoIncrementBiHashMap.java
@@ -0,0 +1,41 @@
+package com.koyomiji.asmweaver;
+
+import com.koyomiji.asmweaver.util.BiHashMap;
+
+public class AutoIncrementBiHashMap<K> extends BiHashMap<K, Integer> {
+  private int nextId = 0;
+
+  public AutoIncrementBiHashMap() {
+    super();
+  }
+
+  public AutoIncrementBiHashMap(AutoIncrementBiHashMap<K> other) {
+    super(other);
+    this.nextId = other.nextId;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public Integer get(Object key) {
+    if (super.containsKey(key)) {
+      return super.get(key);
+    }
+
+    K genericKey = (K) key;
+    Integer newValue = nextId++;
+
+    super.put(genericKey, newValue);
+    return newValue;
+  }
+
+  @Override
+  public Integer put(K key, Integer value) {
+    Integer oldValue = super.put(key, value);
+
+    if (value != null && value >= nextId) {
+      nextId = value + 1;
+    }
+
+    return oldValue;
+  }
+}
---
+++ src/test/java/com/koyomiji/asmweaver/AutoIncrementBiHashMapTest.java
@@ -0,0 +1,28 @@
+package com.koyomiji.asmweaver;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AutoIncrementBiHashMapTest {
+  @Test
+  void test_0() {
+    AutoIncrementBiHashMap<String> map = new AutoIncrementBiHashMap<>();
+    Assertions.assertEquals(0, map.get("a"));
+    Assertions.assertEquals(1, map.get("b"));
+
+    map.put("c", 5);
+    Assertions.assertEquals(5, map.get("c"));
+
+    map.put("d", 6);
+    Assertions.assertEquals(7, map.get("e"));
+  }
+
+  @Test
+  void test_1() {
+    AutoIncrementBiHashMap<String> map = new AutoIncrementBiHashMap<>();
+    map.get("a");
+    AutoIncrementBiHashMap<String> map2 = new AutoIncrementBiHashMap<>(map);
+    Assertions.assertEquals(0, map2.get("a"));
+    Assertions.assertEquals(1, map2.get("b"));
+  }
+}
