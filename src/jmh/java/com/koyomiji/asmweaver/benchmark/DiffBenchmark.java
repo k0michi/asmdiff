ac7764fc2f00bda7c84a58d0390f3d56e632fc61
k0michi <k0michi@koyomi.co>
2026-05-16 12:43:50 +0900

    wip: benchmark

--- src/jmh/java/com/koyomiji/asmweaver/benchmark/DiffBenchmark.java
+++ src/jmh/java/com/koyomiji/asmweaver/benchmark/DiffBenchmark.java
@@ -4,9 +4,7 @@ import com.koyomiji.asmweaver.InsnListDiff;
 import com.koyomiji.asmweaver.InsnListDiffUtils;
 import com.koyomiji.asmweaver.InsnListListAdapter;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.InsnList;
-import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.*;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
@@ -22,6 +20,8 @@ import java.util.concurrent.TimeUnit;
 public class DiffBenchmark {
   private InsnList list1;
   private InsnList list2;
+  private InsnList list3;
+  private InsnList list4;
   @Param({"1000", "10000"})
   public int size;
 
@@ -32,6 +32,23 @@ public class DiffBenchmark {
     for (int i = 0; i < size; i++) {
       list1.add(new InsnNode(Opcodes.NOP));
     }
+
+    list3 = new InsnList();
+    for (int i = 0; i < size; i++) {
+      LabelNode label = new LabelNode();
+      list3.add(label);
+      list3.add(new JumpInsnNode(Opcodes.IFEQ, label));
+    }
+
+    list4 = new InsnList();
+    for (int i = 0; i < size; i++) {
+      LabelNode label = new LabelNode();
+      list4.add(label);
+    }
+
+    for (int i = 0; i < size; i++) {
+      list4.add(new JumpInsnNode(Opcodes.IFEQ, (LabelNode) list4.get(size - 1 - i)));
+    }
   }
 
   @Benchmark
@@ -40,6 +57,28 @@ public class DiffBenchmark {
             new InsnListListAdapter(list1),
             (insn) -> -1,
             new InsnListListAdapter(list2),
+            (insn) -> -1
+    );
+    bh.consume(diff);
+  }
+
+  @Benchmark
+  public void benchmark_diff_matchLabels(Blackhole bh) {
+    InsnListDiff diff = InsnListDiffUtils.diff(
+            new InsnListListAdapter(list3),
+            (insn) -> -1,
+            new InsnListListAdapter(list3),
+            (insn) -> -1
+    );
+    bh.consume(diff);
+  }
+
+  @Benchmark
+  public void benchmark_diff_matchNestedLabels(Blackhole bh) {
+    InsnListDiff diff = InsnListDiffUtils.diff(
+            new InsnListListAdapter(list4),
+            (insn) -> -1,
+            new InsnListListAdapter(list4),
             (insn) -> -1
     );
     bh.consume(diff);
