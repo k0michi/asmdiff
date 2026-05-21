2f6568438c05fb946cf998f8906bf45611cb7c81
k0michi <k0michi@koyomi.co>
2026-05-21 18:16:15 +0900

    test: add

--- src/test/java/com/koyomiji/asmweaver/MethodNodeHelperTest.java
+++ src/test/java/com/koyomiji/asmweaver/MethodNodeHelperTest.java
@@ -4,24 +4,141 @@ import com.koyomiji.asmweaver.util.AutoIncrementBiHashMap;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.TypePath;
+import org.objectweb.asm.TypeReference;
 import org.objectweb.asm.tree.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
 class MethodNodeHelperTest {
   static List<MethodNode> generateUnique() {
+    List<MethodNode> list = new ArrayList<>();
+
     MethodNode node1 = new MethodNode(0, "method", "()V", null, null);
+    list.add(node1);
+
     MethodNode node2 = new MethodNode(0, "method_", "()V", null, null);
+    list.add(node2);
+
     MethodNode node3 = new MethodNode(0, "method", "(I)V", null, null);
+    list.add(node3);
+
     MethodNode node4 = new MethodNode(0, "method", "()V", "()V", null);
+    list.add(node4);
+
     MethodNode node5 = new MethodNode(0, "method", "()V", null, new String[]{"java/lang/Exception"});
+    list.add(node5);
+
     MethodNode node6 = new MethodNode(0, "method", "()V", null, null);
     node6.parameters = List.of(new ParameterNode("param", 0));
+    list.add(node6);
 
-    return List.of(node1, node2, node3, node4, node5, node6);
+    MethodNode node7 = new MethodNode(0, "method", "()V", null, null);
+    node7.visibleAnnotations = List.of(new AnnotationNode("LA;"));
+    list.add(node7);
+
+    MethodNode node8 = new MethodNode(0, "method", "()V", null, null);
+    node8.invisibleAnnotations = List.of(new AnnotationNode("LA;"));
+    list.add(node8);
+
+    MethodNode node9 = new MethodNode(0, "method", "()V", null, null);
+    node9.visibleTypeAnnotations = List.of(
+            new TypeAnnotationNode(TypeReference.CLASS_TYPE_PARAMETER, TypePath.fromString("."), "LA;")
+    );
+    list.add(node9);
+
+    MethodNode node10 = new MethodNode(0, "method", "()V", null, null);
+    node10.invisibleTypeAnnotations = List.of(
+            new TypeAnnotationNode(TypeReference.CLASS_TYPE_PARAMETER, TypePath.fromString("."), "LA;")
+    );
+    list.add(node10);
+
+    MethodNode node11 = new MethodNode(0, "method", "()V", null, null);
+    node11.annotationDefault = 1;
+    list.add(node11);
+
+    MethodNode node12 = new MethodNode(0, "method", "()V", null, null);
+    node12.visibleAnnotableParameterCount = 1;
+    list.add(node12);
+
+    MethodNode node13 = new MethodNode(0, "method", "()V", null, null);
+    node13.visibleParameterAnnotations = new List[]{
+            List.of(new AnnotationNode("LA;"))
+    };
+    list.add(node13);
+
+    MethodNode node14 = new MethodNode(0, "method", "()V", null, null);
+    node14.invisibleAnnotableParameterCount = 1;
+    list.add(node14);
+
+    MethodNode node15 = new MethodNode(0, "method", "()V", null, null);
+    node15.invisibleParameterAnnotations = new List[]{
+            List.of(new AnnotationNode("LA;"))
+    };
+    list.add(node15);
+
+    MethodNode node16 = new MethodNode(0, "method", "()V", null, null);
+    node16.instructions.add(new InsnNode(Opcodes.NOP));
+    list.add(node16);
+
+    LabelNode l0 = new LabelNode(), l1 = new LabelNode(), l2 = new LabelNode();
+    MethodNode node17 = new MethodNode(0, "method", "()V", null, null);
+    node17.instructions.add(l0);
+    node17.instructions.add(new InsnNode(Opcodes.NOP));
+    node17.instructions.add(l1);
+    node17.instructions.add(new InsnNode(Opcodes.NOP));
+    node17.instructions.add(l2);
+    node17.tryCatchBlocks = List.of(
+            new TryCatchBlockNode(l0, l1, l2, null)
+    );
+    list.add(node17);
+
+    MethodNode node18 = new MethodNode(0, "method", "()V", null, null);
+    node18.maxStack = 1;
+    list.add(node18);
+
+    MethodNode node19 = new MethodNode(0, "method", "()V", null, null);
+    node19.maxLocals = 1;
+    list.add(node19);
+
+    l0 = new LabelNode();
+    l1 = new LabelNode();
+    MethodNode node20 = new MethodNode(0, "method", "()V", null, null);
+    node20.instructions.add(l0);
+    node20.instructions.add(new InsnNode(Opcodes.NOP));
+    node20.instructions.add(l1);
+    node20.localVariables = List.of(
+            new LocalVariableNode("name", "desc", "signature", l0, l1, 0)
+    );
+    list.add(node20);
+
+    l0 = new LabelNode();
+    l1 = new LabelNode();
+    MethodNode node21 = new MethodNode(0, "method", "()V", null, null);
+    node21.instructions.add(l0);
+    node21.instructions.add(new InsnNode(Opcodes.NOP));
+    node21.instructions.add(l1);
+    node21.visibleLocalVariableAnnotations = List.of(
+            new LocalVariableAnnotationNode(TypeReference.CLASS_TYPE_PARAMETER, TypePath.fromString("."), new LabelNode[]{l0}, new LabelNode[]{l1}, new int[]{0}, "LA;")
+    );
+    list.add(node21);
+
+    l0 = new LabelNode();
+    l1 = new LabelNode();
+    MethodNode node22 = new MethodNode(0, "method", "()V", null, null);
+    node22.instructions.add(l0);
+    node22.instructions.add(new InsnNode(Opcodes.NOP));
+    node22.instructions.add(l1);
+    node22.invisibleLocalVariableAnnotations = List.of(
+            new LocalVariableAnnotationNode(TypeReference.CLASS_TYPE_PARAMETER, TypePath.fromString("."), new LabelNode[]{l0}, new LabelNode[]{l1}, new int[]{0}, "LA;")
+    );
+    list.add(node22);
+
+    return list;
   }
 
   @Test
